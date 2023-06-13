8c768dfb9398c6d6264643a855975bc28a369ec1
sadamczyk <stasinho.seklemow.2001@gmail.com>
2023-06-13 16:05:20 +0200

    FINAL version of predictor creating function, creating sets with edi sum and predictors and applying Janek's function for robust reggresion

--- R/robust_models.R
+++ R/robust_models.R
@@ -1,7 +1,8 @@
 library(robustbase)
 library(car)
+library(dplyr)
 d <- read.csv('./data/dane_poprawione.csv') # . is the home directory 
-
+d <- select(d, -starts_with(c("OtherPlaces", "OtherReasons")))
 # make a substet of the d data.frame with predictor (EDI_SUM) and predictors of choosing 
 reasons_lsd <- subset(d, select = c("EDI_SUM", 
                                     "ReasonsLSD.SQ001.", 
@@ -22,31 +23,21 @@ places_lsd <- subset(d, select = c("EDI_SUM",
                                    "PlacesLSD.SQ006.",
                                    "PlacesLSD.SQ007."))
 #function to quickly generate predictor names from our database
-
 create_predictors <- function(condition, substance) {
-  if (condition == 'places') {
-    predictors <- c(paste0("Places", substance, ".SQ00", 1:7, "."))
-  } else if (condition == 'reasons') {
-    predictors <- c(paste0("Reasons", substance, ".SQ00", 1:8, "."))
-  } else if (condition == 'people') {
-    predictors <- c(paste0("WithWhom", substance, ".SQ00", 1:6, "."))
-  }
+  require(dplyr)
+  cmax <- length(select(d, contains(condition) & contains(substance)))
+  predictors <- c(paste0(condition, substance, ".SQ00", 1:cmax, "."))
   predictors
-} 
+}
+###################################
+#creating sets containing predictors and predicted value
+reasons_lsd <- subset(d, select = c("EDI_SUM", create_predictors('Reasons', 'LSD')))
+places_lsd <- subset(d, select = c("EDI_SUM", create_predictors('Places', 'LSD')))
+with_whom_lsd <- subset(d, select = c("EDI_SUM", create_predictors('WithWhom', 'LSD')))
+reasons_mushrooms <- subset(d, select = c("EDI_SUM", create_predictors('Reasons', 'Mushrooms')))
+places_mushrooms <- subset(d, select = c("EDI_SUM", create_predictors('Places', 'Mushrooms')))
+with_whom_mushrooms <- subset(d, select = c("EDI_SUM", create_predictors('WithWhom', 'Mushrooms')))
 
-####################testing. I'll delete it later
-test_places_lsd <- create_predictors('places', "LSD")
-test_places_lsd == c("PlacesLSD.SQ001.", "PlacesLSD.SQ002.", "PlacesLSD.SQ003.", "PlacesLSD.SQ004.", "PlacesLSD.SQ005.",
-                     "PlacesLSD.SQ006.", "PlacesLSD.SQ007.")
-
-test_reasons_lsd <- create_predictors('reasons', "LSD")
-test_reasons_lsd == c("ReasonsLSD.SQ001.", "ReasonsLSD.SQ002.", "ReasonsLSD.SQ003.", "ReasonsLSD.SQ004.",
-                      "ReasonsLSD.SQ005.", "ReasonsLSD.SQ006.", "ReasonsLSD.SQ007.", "ReasonsLSD.SQ008.")
-
-test_people_lsd <- create_predictors('people', "LSD")
-test_people_lsd == c("WithWhomLSD.SQ001.", "WithWhomLSD.SQ002.", "WithWhomLSD.SQ003.", "WithWhomLSD.SQ004.", 
-                     "WithWhomLSD.SQ005.", "WithWhomLSD.SQ006.")
-#################################
 
 # function to fit robust linear regression 
 # takes the subsetted data and returns the model
@@ -60,8 +51,12 @@ fit_robust_lm <- function(.data) {
 
 # use of the function
 reasons_lsd_model <- fit_robust_lm(reasons_lsd) 
-  
-places_lsd_model <- (places_lsd)
+places_lsd_model <- fit_robust_lm(places_lsd) 
+with_whom_lsd_model <- fit_robust_lm(with_whom_lsd) 
+reasons_mushrooms_model <- fit_robust_lm(reasons_mushrooms) 
+places_mushrooms_model <- fit_robust_lm(places_mushrooms) 
+with_whom_mushrooms_model <- fit_robust_lm(with_whom_mushrooms) 
+
 
 # use with lapply wrapper
 # takes a list of subsetted datasets and returns a list of models 
