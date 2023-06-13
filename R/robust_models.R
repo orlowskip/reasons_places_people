bf408541c97be5576ce3447ea2540970d89485cd
sadamczyk <stasinho.seklemow.2001@gmail.com>
2023-06-13 13:45:37 +0200

    More general function creating all kinds of predictors without using for loop.

--- R/robust_models.R
+++ R/robust_models.R
@@ -22,19 +22,31 @@ places_lsd <- subset(d, select = c("EDI_SUM",
                                    "PlacesLSD.SQ006.",
                                    "PlacesLSD.SQ007."))
 #function to quickly generate predictor names from our database
-# I had to create three separate functions cause places, reasons and people differ in number of questions so loop would have not work
-create_places <- function(substance) {
-  predictors <- c()
-  for (x in 1:7) {
-    predictors <- c(predictors, paste0("Places", substance, ".SQ00", as.character(x), "."))
+
+create_predictors <- function(condition, substance) {
+  if (condition == 'places') {
+    predictors <- c(paste0("Places", substance, ".SQ00", 1:7, "."))
+  } else if (condition == 'reasons') {
+    predictors <- c(paste0("Reasons", substance, ".SQ00", 1:8, "."))
+  } else if (condition == 'people') {
+    predictors <- c(paste0("WithWhom", substance, ".SQ00", 1:6, "."))
   }
   predictors
 }
 
-test_places_lsd <- create_places("LSD")
-test_places_lsd
+####################testing. I'll delete it later
+test_places_lsd <- create_predictors('places', "LSD")
+test_places_lsd == c("PlacesLSD.SQ001.", "PlacesLSD.SQ002.", "PlacesLSD.SQ003.", "PlacesLSD.SQ004.", "PlacesLSD.SQ005.",
+                     "PlacesLSD.SQ006.", "PlacesLSD.SQ007.")
 
+test_reasons_lsd <- create_predictors('reasons', "LSD")
+test_reasons_lsd == c("ReasonsLSD.SQ001.", "ReasonsLSD.SQ002.", "ReasonsLSD.SQ003.", "ReasonsLSD.SQ004.",
+                      "ReasonsLSD.SQ005.", "ReasonsLSD.SQ006.", "ReasonsLSD.SQ007.", "ReasonsLSD.SQ008.")
 
+test_people_lsd <- create_predictors('people', "LSD")
+test_people_lsd == c("WithWhomLSD.SQ001.", "WithWhomLSD.SQ002.", "WithWhomLSD.SQ003.", "WithWhomLSD.SQ004.", 
+                     "WithWhomLSD.SQ005.", "WithWhomLSD.SQ006.")
+#################################
 
 # function to fit robust linear regression 
 # takes the subsetted data and returns the model
