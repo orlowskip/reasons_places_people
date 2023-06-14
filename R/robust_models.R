a87c40f689fb2d4f7f9ccf16418e2aa5eff7e712
Your <jan.szczypinski@gmail.com>
2023-06-14 16:15:18 +0200

    The functions are now sourced from "./R/functions.R" file Changed the subset generation to a for loop, which creates a list of substets. Next the models are created using the lapply wrapper.

--- R/robust_models.R
+++ R/robust_models.R
@@ -1,97 +1,42 @@
-library(robustbase)
 library(car)
 library(dplyr)
+# load the functions used in the script
+# create_predictors()
+# and fit_robust_lm
+source("./R/functions.R")
+# read and clean the data 
 d <- read.csv('./data/dane_poprawione.csv') # . is the home directory 
 d <- select(d, -starts_with(c("OtherPlaces", "OtherReasons")))
-# make a substet of the d data.frame with predictor (EDI_SUM) and predictors of choosing 
-reasons_lsd <- subset(d, select = c("EDI_SUM", 
-                                    "ReasonsLSD.SQ001.", 
-                                    "ReasonsLSD.SQ002.", 
-                                    "ReasonsLSD.SQ003.",
-                                    "ReasonsLSD.SQ004.",
-                                    "ReasonsLSD.SQ005.",
-                                    "ReasonsLSD.SQ006.",
-                                    "ReasonsLSD.SQ007.",
-                                    "ReasonsLSD.SQ008."))
-# same as above
-places_lsd <- subset(d, select = c("EDI_SUM", 
-                                   "PlacesLSD.SQ001.", 
-                                   "PlacesLSD.SQ002.", 
-                                   "PlacesLSD.SQ003.",
-                                   "PlacesLSD.SQ004.",
-                                   "PlacesLSD.SQ005.",
-                                   "PlacesLSD.SQ006.",
-                                   "PlacesLSD.SQ007."))
-#function to quickly generate predictor names from our database
-create_predictors <- function(condition, substance) {
-  require(dplyr)
-  cmax <- length(select(d, contains(condition) & contains(substance)))
-  predictors <- c(paste0(condition, substance, ".SQ00", 1:cmax, "."))
-  predictors
-}
-###################################
-#creating sets containing predictors and predicted value
-reasons_lsd <- subset(d, select = c("EDI_SUM", create_predictors('Reasons', 'LSD')))
-places_lsd <- subset(d, select = c("EDI_SUM", create_predictors('Places', 'LSD')))
-with_whom_lsd <- subset(d, select = c("EDI_SUM", create_predictors('WithWhom', 'LSD')))
-reasons_mushrooms <- subset(d, select = c("EDI_SUM", create_predictors('Reasons', 'Mushrooms')))
-places_mushrooms <- subset(d, select = c("EDI_SUM", create_predictors('Places', 'Mushrooms')))
-with_whom_mushrooms <- subset(d, select = c("EDI_SUM", create_predictors('WithWhom', 'Mushrooms')))
 
-
-#function to quickly generate predictor names from our database
-# I had to create three separate functions cause places, reasons and people differ in number of questions so loop would have not work
-create_places <- function(substance) {
-  predictors <- c()
-  for (x in 1:7) {
-    predictors <- c(predictors, paste0("Places", substance, ".SQ00", x, "."))
+# maybe we can use a for loop to make it cleaner
+# conditions names
+conditions <- c("Reasons", "Places", "WithWhom")
+# substances names
+substances <- c("LSD", "Mushrooms")
+# empty list for filling with subsets
+subsets <- list()
+# empty vector for filling with names 
+names <- c()
+# loop over the substances
+for (i in substances) {
+  # and over the conditions
+  for (j in conditions) {
+    # make a vector of names to name the list of subsets
+    subset_names <- c(subset_names, paste(j, i, sep = "_"))
+    # create a temp subset and coerce to a list
+    temp_subset <- list(subset(d,
+                          select = c("EDI_SUM",
+                                     create_predictors(j, i))))
+    # append subsets to a list
+    subsets <- append(subsets,
+                   temp_subset)
   }
-  predictors
-}
-
-
-create_predictors <- function(condition, substance) {
-  require(dplyr)
-  cmax <- length(select(d, contains(condition) & contains(substance)))
-  predictors <- c(paste0(condition, substance, ".SQ00", 1:cmax, "."))
-  predictors
-}
-
-paste0("Places", "LSD", ".SQ00", 1:7, ".")
-
-test_places_lsd <- create_places("LSD")
-test_places_lsd
-
-
-
-
-# function to fit robust linear regression 
-# takes the subsetted data and returns the model
-fit_robust_lm <- function(.data) {
-  model <- lmrob(EDI_SUM ~ ., data = .data, # EDI_SUM ~ . means "predict edi using all other vars in the data 
-                 na.action = na.omit, 
-                 fast.s.large.n = Inf, 
-                 setting = "KS2014")
-  model
-}
-
-# use of the function
-reasons_lsd_model <- fit_robust_lm(reasons_lsd) 
-places_lsd_model <- fit_robust_lm(places_lsd) 
-with_whom_lsd_model <- fit_robust_lm(with_whom_lsd) 
-reasons_mushrooms_model <- fit_robust_lm(reasons_mushrooms) 
-places_mushrooms_model <- fit_robust_lm(places_mushrooms) 
-with_whom_mushrooms_model <- fit_robust_lm(with_whom_mushrooms) 
-
-
-# use with lapply wrapper
-# takes a list of subsetted datasets and returns a list of models 
-models <- lapply(list(reasons_lsd, places_lsd), fit_robust_lm)
-
-# Anova() function from car package will return anova table from the model
-Anova(reasons_lsd_model, type = "III") # type = chooses the type of sum of squares calculations, use type = "III" 
-Anova(places_lsd, type = "III") 
-
-# can use with lapply as well
+} 
+# name the subsets in the list
+names(subsets) = subset_names
+# and now we can use lapply wrapper to do the models in one line
+# lapply is a wrapper that goes through every element in the list and apply the function
+models <- lapply(subsets, fit_robust_lm)
+# and then anovas tables
 anovas <- lapply(models, Anova, type = "III")
 
